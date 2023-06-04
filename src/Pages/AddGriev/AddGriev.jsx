132b65ea786a122cbc4ec266171633a9abad89e8
Abhishek <you@example.com>
2023-06-04 20:14:41 +0530

    api-int3

--- src/Pages/AddGriev/AddGriev.jsx
+++ src/Pages/AddGriev/AddGriev.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import './AddGriev.css'
+import { TextField } from '@mui/material'
 
 function AddGriev() {
-  return (
-    <div className='addgriev'>
-        <div className="add-row">
-            <span className="add-title">Grievence Form</span>
+    return (
+        <div className='addgriev'>
+            <div className="add-row">
+                <span className="add-title">Grievence Form</span>
+            </div>
+            <div className="add-row">
+                <div className="add-box">
+                    <div className="add-box-row">
+                        <div className="add-box-col">
+                            <div className="add-box-item-row">
+                                <TextField className='add-griev-input subject' label='Subject' type='text' />
+                            </div>
+                        </div>
+                        <div className="add-box-col">
+                            <div className="add-box-item-row">
+                                <TextField className='add-griev-input date' label='Date' type='text' />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="add-box-row">
+                        <div className="add-box-item-row">
+                            <TextField className='add-griev-input desc' multiline
+                                rows={8}
+                                maxRows={8} label='Description' type='text' />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="add-row"></div>
-    </div>
-  )
+    )
 }
 
 export default AddGriev
